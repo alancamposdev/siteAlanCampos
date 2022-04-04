f8d424ab4224001492ded94427efdc1cd8798882
AlanUbuntu <camposalan77@gmail.com>
2022-04-03 21:45:56 -0400

    adicionei botão de whatsApp

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
         integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Google fontes-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <!--Animate CSS -->
@@ -24,6 +25,7 @@
     <!--Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!-- Styles Main-->
     <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
@@ -39,7 +41,12 @@
 
     <x-footer />
 
-    <x-btnScrool /> 
+    <x-btnScrool />
+    {{-- https://wa.me/55(aqui seu numero com ddd | tudo junto)?text=Adorei%20seu%20artigo --}}
+
+    <a href="https://api.whatsapp.com/send?phone=556984067550" target="_blank" class="btnWhats animate__animated animate__bounce animate__repeat-3 3">
+    <i style="margin-top:16px" class="fa fa-whatsapp"></i>
+    </a>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
